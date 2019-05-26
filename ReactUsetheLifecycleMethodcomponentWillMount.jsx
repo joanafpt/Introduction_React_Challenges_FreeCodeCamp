382cb876998a0950c4dbc9d3133e64bd2f00e064
Joana <joanajteixeira@outlook.com>
2019-05-26 12:23:52 +0100

    React: Use the Lifecycle Method componentWillMount
    /* The componentWillMount() method is called before the render() method when a component is being mounted to the DOM. Log something to the console within componentWillMount() - you may want to have your browser console open to see the output.*/

---
+++ ReactUsetheLifecycleMethodcomponentWillMount.jsx
@@ -0,0 +1,17 @@
+//React: Use the Lifecycle Method componentWillMount
+/* The componentWillMount() method is called before the render() method when a component is being mounted to the DOM. Log something to the console within componentWillMount() - you may want to have your browser console open to see the output.*/
+
+
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    componentWillMount() {
+      // change code below this line
+        console.log("Hello");
+      // change code above this line
+    }
+    render() {
+      return <div />
+    }
+  };
