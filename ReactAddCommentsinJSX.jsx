8d8899b478ae045e46867facd7c868964bdb55db
Joana <joanajteixeira@outlook.com>
2019-05-26 11:57:21 +0100

    //React: Add Comments in JSX

---
+++ ReactAddCommentsinJSX.jsx
@@ -0,0 +1,9 @@
+//React: Add Comments in JSX
+
+const JSX = (
+    <div>
+        <h1>This is a block of JSX</h1>
+        <p>Here's a subtitle</p>
+    /*Is that any comment out there?*/
+    </div>
+);
