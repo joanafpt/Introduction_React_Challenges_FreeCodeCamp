c2b1a860b91764c2ab8489da299cfe336055774c
Joana <joanajteixeira@outlook.com>
2019-05-26 11:57:59 +0100

    React: Add Inline Styles in React
    /*If you have a large set of styles, you can assign a style object to a constant to keep your code organized. Uncomment the styles constant and declare an object with three style properties and their values. Give the div a color of "purple", a font-size of 40, and a border of "2px solid purple". Then set the style attribute equal to the styles constant. */

---
+++ ReactAddInlineStylesinReact.jsx
@@ -0,0 +1,16 @@
+//React: Add Inline Styles in React
+/*If you have a large set of styles, you can assign a style object to a constant to keep your code organized. Uncomment the styles constant and declare an object with three style properties and their values. Give the div a color of "purple", a font-size of 40, and a border of "2px solid purple". Then set the style attribute equal to the styles constant. */
+
+
+
+const styles = { color: "purple", fontSize: 40, border: "2px solid purple" };
+// change code above this line
+class Colorful extends React.Component {
+  render() {
+    // change code below this line
+    return (
+      <div style={styles}>Style Me!</div>
+    );
+    // change code above this line
+  }
+};
