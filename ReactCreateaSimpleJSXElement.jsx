91e83f9d26dc25dd4d220066ba80691297a2fc60
Joana <joanajteixeira@outlook.com>
2019-05-26 12:10:44 +0100

    React: Create a Simple JSX Element
    /*Instructions: The current code uses JSX to assign a div element to the constant JSX. Replace the div with an h1 element and add the text Hello JSX! inside it.
    */

---
+++ ReactCreateaSimpleJSXElement.jsx
@@ -0,0 +1,6 @@
+//React: Create a Simple JSX Element
+
+/*Instructions: The current code uses JSX to assign a div element to the constant JSX. Replace the div with an h1 element and add the text Hello JSX! inside it.
+*/
+
+const JSX = <h1>Hello JSX!</h1>;
