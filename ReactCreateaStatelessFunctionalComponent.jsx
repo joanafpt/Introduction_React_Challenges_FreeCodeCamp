81df36a2829f003bf35e8c6996f474f76e45577c
Joana <joanajteixeira@outlook.com>
2019-05-26 12:11:15 +0100

    React: Create a Stateless Functional Component
    /*
    The code editor has a function called MyComponent. Complete this function so it returns a single div element which contains some string of text.
    
    Note: The text is considered a child of the div element, so you will not be able to use a self-closing tag.
    */

---
+++ ReactCreateaStatelessFunctionalComponent.jsx
@@ -0,0 +1,15 @@
+//React: Create a Stateless Functional Component
+/*
+The code editor has a function called MyComponent. Complete this function so it returns a single div element which contains some string of text.
+
+Note: The text is considered a child of the div element, so you will not be able to use a self-closing tag.
+*/
+const MyComponent = function() {
+  // change code below this line
+  return (
+    <div> text= "stringOfText" </div>
+  );
+
+
+  // change code above this line
+}
