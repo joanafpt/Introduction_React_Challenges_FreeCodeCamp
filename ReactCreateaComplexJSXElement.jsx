6cc7db9e0f46591c551623a64253cc019acb4d8e
Joana <joanajteixeira@outlook.com>
2019-05-26 11:59:29 +0100

    React: Create a Complex JSX Element
    /*Define a new constant JSX that renders a div which contains the following elements in order:
    An h1, a p, and an unordered list that contains three li items. You can include any text you want within each element.
    */

---
+++ ReactCreateaComplexJSXElement.jsx
@@ -0,0 +1,15 @@
+/* React: Create a Complex JSX Element
+Define a new constant JSX that renders a div which contains the following elements in order:
+An h1, a p, and an unordered list that contains three li items. You can include any text you want within each element.
+*/
+
+// write your code here
+const JSX = <div>
+    <h1>Hello </h1>
+    <p> hello</p>
+    <ul>
+        <li>hellooo</li>
+        <li>petite</li>
+        <li>demoiselle</li>
+    </ul>
+</div>
